cd1c72635ffe47404130f16b6333333fa614d057
zjayee <jayee.zheng@gmail.com>
2023-11-10 01:56:18 -0500

    Set up two way websocket connection

--- src/websocket.js
+++ src/websocket.js
@@ -1,35 +1,47 @@
+import { Client } from "@stomp/stompjs";
+import websocket from "websocket";
 
-import { Client } from '@stomp/stompjs';
-import websocket from 'websocket';
+Object.assign(global, { WebSocket: websocket.w3cwebsocket });
 
-Object.assign(global, { WebSocket: websocket.w3cwebsocket })
+var client;
+var role;
+var sessionId;
 
-const client = new Client({
-  brokerURL: 'ws://localhost:8080/mywebsockets',
-  onConnect: () => {
-    client.subscribe('/topic/messages', message =>
-      console.log(`Received: ${message.body}`)
-    );
+export function connectWebsocket(userRole, sessionID) {
+  role = userRole;
+  sessionId = sessionID;
 
-    client.onWebSocketError = (error) => {
-        console.error('Error with websocket', error);
-    }
-    
-    client.onStompError = (frame) => {
-        console.error('Broker reported error: ' + frame.headers['message']);
-        console.error('Additional details: ' + frame.body);
-    };
+  client = new Client({
+    brokerURL: "ws://localhost:8080/callhub",
+    onConnect: () => {
+      client.subscribe(`/topic/message-${role}/${sessionId}`, (message) => {
+        // TODO: handle received message
+        console.log(`Received: ${message.body}`);
+      });
 
-    client.publish({ destination: '/app/message', body: 'First Message' });
-  },
-});
+      client.onWebSocketError = (error) => {
+        console.error("Error with websocket", error);
+      };
 
-export function connect(){ 
-    client.activate();
+      client.onStompError = (frame) => {
+        console.error("Broker reported error: " + frame.headers["message"]);
+        console.error("Additional details: " + frame.body);
+      };
+
+      console.log("connected to websocket");
+    },
+  });
+  client.activate();
 }
 
-export function disconnect(){
-    client.deactivate()
-    console.log("websocket disconnected");
+export function disconnectWebsocket() {
+  client.deactivate();
+  console.log("websocket disconnected");
 }
 
+export function sendMessageWebsocket(message) {
+  client.publish({
+    destination: `/app/message-${role}/${sessionId}`,
+    body: message,
+  });
+}
