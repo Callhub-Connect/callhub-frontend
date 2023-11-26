afee1aef65e0c44fb651f50d75b930373761a9bb
3m1ly03 <emilyfaithzhou@icloud.com>
2023-11-26 18:14:47 -0500

    Fixed routing for chat wrapper

--- src/components/chat-component/Chat.wrapper.jsx
+++ src/components/chat-component/Chat.wrapper.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
 import Chat from './Chat';
 import Welcome from '../welcome-component/Welcome';
 import Alert from '@mui/material/Alert';
@@ -6,6 +7,7 @@ import Snackbar from '@mui/material/Snackbar';
 
 const ChatWrapper = () => {
   const [open, setOpen] = useState(true);
+  const navigate = useNavigate();
   const isSessionActive = localStorage.getItem('isSessionActive') === 'true';
 
   useEffect(() => {
@@ -16,6 +18,17 @@ const ChatWrapper = () => {
     return () => clearTimeout(timer);
   }, []);
 
+  useEffect(() => {
+    if (!isSessionActive) {
+      // Show the Snackbar for a short period before navigating
+      const navigateTimer = setTimeout(() => {
+        navigate("/");
+      }, 2500); // Delay the navigation
+
+      return () => clearTimeout(navigateTimer);
+    }
+  }, [isSessionActive, navigate]);
+
   if (!isSessionActive) {
     return (
       <>
@@ -23,7 +36,7 @@ const ChatWrapper = () => {
           open={open} 
           anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
         >
-          <Alert severity="error" sx={{ fontSize: '1.5rem', }}>
+          <Alert severity="error" sx={{ fontSize: '1.5rem' }}>
             Please join an active session
           </Alert>
         </Snackbar>
@@ -37,7 +50,7 @@ const ChatWrapper = () => {
           open={open} 
           anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
         >
-          <Alert severity="success" sx={{ fontSize: '1.5rem', }}>
+          <Alert severity="success" sx={{ fontSize: '1.5rem' }}>
             Connected to session
           </Alert>
         </Snackbar>
