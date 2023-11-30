4699a0b8a890e542cf55dfdd1927e06e716f4b1c
3m1ly03 <emilyfaithzhou@icloud.com>
2023-11-29 22:39:08 -0500

    Changed width and height of pdf view

--- src/helper-components/pdf-manager-component/PdfViewerComponent.jsx
+++ src/helper-components/pdf-manager-component/PdfViewerComponent.jsx
@@ -33,45 +33,7 @@ export default function PdfViewerComponent(props) {
 	return (
 		<div
 			ref={containerRef}
-			style={{ width: '100%', height: '80%' }}
-		/>
-	);
-}import { useEffect, useRef } from 'react';
-
-export default function PdfViewerComponent(props) {
-	const containerRef = useRef(null);
-
-	useEffect(() => {
-		console.log("loeading new")
-		const container = containerRef.current;
-		// eslint-disable-next-line no-unused-vars
-		let instance, PSPDFKit;
-		(async function () {
-			PSPDFKit = await import('pspdfkit');
-
-			PSPDFKit.unload(container); // Ensure that there's only one PSPDFKit instance.
-
-			instance = await PSPDFKit.load({
-				// Container where PSPDFKit should be mounted.
-				container,
-				// The document to open.
-				document: props.document,
-				// Use the public directory URL as a base URL. PSPDFKit will download its library assets from here.
-				baseUrl: `${window.location.protocol}//${window.location.host}/${process.env.PUBLIC_URL}`,
-			});
-		})();
-
-		return () => PSPDFKit && PSPDFKit.unload(container);
-	}, [props]);
-	
-	// const instance = PSPDFKit.instance.default;
-	// instance.exportPDF().then(arrayBuffer => {
-	// const blob = new Blob ( [arrayBuffer], { type: "application/pdf" });
-	// });
-	return (
-		<div
-			ref={containerRef}
-			style={{ width: '100%', height: '100%' }}
+			style={{ width: '99%', height: '100%' }}
 		/>
 	);
 }
